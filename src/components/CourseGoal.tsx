3a22ad3b13f74077a40e16bcf7cd678f1422774a
DELL <DELL@DESKTOP-VLVF8CL>
2025-08-23 20:12:10 +0530

    Props

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,12 @@
-export default function CourseGoal() {
+export default function CourseGoal( {title, description}: 
+    {title: string; description: string
+    }) 
+{
     return (
     <article>
         <div>
-            <h2>Title</h2>
-            <p>Description</p>
+            <h2>{title}</h2>
+            <p>{description}</p>
         </div>
         <button>Delete</button>
         </article>
