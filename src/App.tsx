53abdedd244c772415db12ede95a8ef193f1cb4e
DELL <DELL@DESKTOP-VLVF8CL>
2025-08-24 12:07:53 +0530

    State Set Goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import CourseGoal from "./components/CourseGoal.tsx";
 import  Header from "./components/Header.tsx";
 import  goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+type CourseGoalProps = {
+  title:string;
+  Desccription:string;
+  id:number;
+}
 
 export default function App() {
+
+   const[goals, setGoals] = useState<CourseGoalProps[]>([]);
+
+  function handleAddGlobal()
+  {
+    setGoals((prevGoals) => {
+      const newGoal:CourseGoalProps = {
+        title:'New Goal',
+        Desccription:'New Description',
+        id: Math.floor(Math.random() * 10000)
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
 
       <Header image={{ src:goalsImg, alt:'A List of goals'}}  >
         <h1>your Course Goals</h1>
       </Header>
-
-      <CourseGoal title ="Learn React+TS">
-          <p>Learn it from the ground up</p>
+      <button onClick={handleAddGlobal} >Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+             <CourseGoal title ={goal.title} >
+          <p>{goal.Desccription}</p>
       </CourseGoal>
+          </li>
+        ))}
+      </ul>
+
+     
     </main>
   );
 }
