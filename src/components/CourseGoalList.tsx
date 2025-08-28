be79f839cebeb55d7ac91a0805b38b571939749f
DELL <DELL@DESKTOP-VLVF8CL>
2025-08-29 05:21:57 +0530

    03 Discriminated Union

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -28,7 +28,7 @@ export default function CourseGoalList( {goals,onDeleteGoal}:CourseGoalListProps
 
   if (goals.length >= 4) {
     warningBox = (
-      <InfoBox mode="warning">
+      <InfoBox mode="warning" severity="medium">
         You're collecting a lot of goals. Don't put too much on your plate!
       </InfoBox>
     );
--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,12 +1,23 @@
 import { type ReactNode } from "react";
 
-type InfoBoxProps =
-{
-    mode:'hint' | 'warning';
-    children:ReactNode;
-}
+type HintboxProps = {
+    mode: 'hint';
+    children: ReactNode;
+};
 
-export default function InfoBox({ mode, children }: InfoBoxProps) {
+type WarningBoxProps =
+{
+    mode:'warning';
+    severity: 'low' | 'medium' | 'high';
+    children:ReactNode;
+};
+
+type InfoBoxProps  = HintboxProps | WarningBoxProps
+
+export default function InfoBox(props: InfoBoxProps ) {
+
+    const {children, mode} = props;
+
   if (mode === 'hint') {
     return (
       <aside className="infobox infobox-hint">
@@ -15,8 +26,10 @@ export default function InfoBox({ mode, children }: InfoBoxProps) {
     );
   }
 
+  const severity = props;
+
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
