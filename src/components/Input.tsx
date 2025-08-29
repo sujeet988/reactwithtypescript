5dfbe6309bd0bb1846db88799276c79bbbc19709
DELL <DELL@DESKTOP-VLVF8CL>
2025-08-29 06:04:43 +0530

    Wrapper with ComponentProps with type

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,18 @@
+import { type ComponentPropsWithoutRef} from 'react';
+
 type InputProps = 
 {
     label: string;
     id:string;
 
-}
+} & ComponentPropsWithoutRef<'input'>;
 
-export default function Input({label,id}:InputProps)
+export default function Input({label,id,...props}:InputProps)
 {
     return(
      <p>
         <label htmlFor ={id}>{label}</label>
-        <input id={id} type="text"></input>
+        <input id={id} {...props}></input>
      </p>
 
     );
